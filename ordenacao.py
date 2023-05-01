d6ea472189e7bcc1325f71d6c9ddae206bab45cb
Anderson Ferreira <97910606+4ndferreira@users.noreply.github.com>
2023-05-01 15:16:50 -0300

    Verificando ordenação
    Receber 3 números inteiros na entrada e imprimir "crescente" se eles forem dados em ordem crescente. Caso contrário, imprimir "não está em ordem crescente".

---
+++ ordenacao.py
@@ -0,0 +1,8 @@
+x=float(input("Digite um número inteiro:"))
+y=float(input("Digite um número inteiro:"))
+z=float(input("Digite um número inteiro:"))
+
+if(x<y<z):
+	print("crescente")
+else:
+	print("não está em ordem crescente")
